b7bb50704e8fb53a233116fcb5577f7cafe8af4a
dicekey0303 <dicekey0303@gmail.com>
2022-09-15 10:35:29 +0900

    heroku接続調整

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
-//おまじない(リクエストのBODYから取得する)
+//おまじない(リクエストのパラメーターがBODYに入るのでBODYから取得するコマンドが必要)
 app.use(express.urlencoded({extended:false}));
 //htmlのフォルダを指定する
 app.use(express.static(path.join(__dirname,"public")));
